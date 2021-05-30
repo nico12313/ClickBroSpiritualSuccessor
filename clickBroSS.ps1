53e83b723e1338407794fdc3d81a75b35112e1be
NicholasWu <james12313@gmail.com>
2021-05-31 04:26:22 +0800

    First commit
    Main script

---
+++ clickBroSS.ps1
@@ -0,0 +1,317 @@
+﻿#Author:地表上最強男人ü 2021-05-30
+#Everyone is wellcome to contribute except those who abuse it
+
+#Process Name:
+#Garena
+#LeagueClient
+#LeagueClientUx
+#League of Legends
+
+#Window Name:
+#Garena - 遊戲中心
+#League of Legends
+#League of Legends (TM) Client
+
+#coordinates:
+#$coordClient =  @( 160 , 70 )
+$sizeClient =    @( 1600, 900 )
+$queueStart =    @( 728 , 841 )
+$queueAccept =   @( 790 , 700 )
+#$coordGame =    @( 0   , 0 )
+$sizeGame =      @( 1920, 1080 )
+$slot1 =         @( 570 , 1000 )
+$slot2 =         @( 784 , 1000 )
+$slot3 =         @( 982 , 1000 )
+$slot4 =         @( 1187, 1000 )
+$slot5 =         @( 1384, 1000 )
+$buttonConfig =  @( 1904, 886 )
+$buttonSurrend = @( 767 , 845 )
+$buttonConfirm = @( 835 , 485 )
+$buttonCancel =  @( 1289, 846 )
+$battlefield1 =  @( 1306, 651 )
+$battlefield2 =  @( 1304, 237 )
+$battlefield3 =  @( 598 , 240 )
+$battlefield4 =  @( 1306, 651 )
+
+#don't know what this does but can't delete
+[System.Reflection.Assembly]::LoadWithPartialName("system.windows.forms")
+
+$targetClient = "LeagueClient"
+$targetGame = "League of Legends"
+$processClient = Get-Process | Where-Object {$_.ProcessName -eq $targetClient}
+$processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+
+#$focusWindow = New-Object -ComObject wscript.shell
+#use case:
+#$focusWindow.AppActivate('League of Legends')
+
+function Show-Window {
+  param(
+    [Parameter(Mandatory)]
+    [string] $ProcessName
+  )
+
+  # As a courtesy, strip '.exe' from the name, if present.
+  $ProcessName = $ProcessName -replace '\.exe$'
+
+  # Get the ID of the first instance of a process with the given name
+  # that has a non-empty window title.
+  # NOTE: If multiple instances have visible windows, it is undefined
+  #       which one is returned.
+  $procId = (Get-Process -ErrorAction Ignore $ProcessName).Where({ $_.MainWindowTitle }, 'First').Id
+
+  if (-not $procId) { Throw "No $ProcessName process with a non-empty window title found." }
+
+  # Note: 
+  #  * This can still fail, because the window could have been closed since
+  #    the title was obtained.
+  #  * If the target window is currently minimized, it gets the *focus*, but is
+  #    *not restored*.
+  #  * The return value is $true only if the window still existed and was *not
+  #    minimized*; this means that returning $false can mean EITHER that the
+  #    window doesn't exist OR that it just happened to be minimized.
+  $null = (New-Object -ComObject WScript.Shell).AppActivate($procId)
+
+}
+#use case:
+#Show-Window "LeagueClientUx"
+
+function moveCursorToSpot{
+param($targetX, $targetY, $target)
+for($i=100;$i -ge 1;$i--){
+$Position = [system.windows.forms.cursor]::Position
+$PositionChangeX = ($targetX - $Position.x)/$i
+$PositionChangeY = ($targetY - $Position.y)/$i
+
+[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Position.x + $PositionChangeX), ($Position.y + $PositionChangeY))
+
+if($i -ge 2){start-sleep -m 5}
+}
+start-sleep -m 20
+}
+#use case:
+#moveCursorToSpot -targetX 960 -targetY 540
+
+function Click-MouseButtonLeft{
+    $signature=@' 
+      [DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+      public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@ 
+
+    $SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
+
+        $SendMouseClick::mouse_event(0x00000002, 0, 0, 0, 0);
+        start-sleep -m 20
+        $SendMouseClick::mouse_event(0x00000004, 0, 0, 0, 0);
+}
+
+#use case:
+#Click-MouseButtonLeft
+
+function Click-MouseButtonRight
+{
+    $signature=@' 
+      [DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+      public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@ 
+
+    $SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
+
+        $SendMouseClick::mouse_event(0x00000008, 0, 0, 0, 0);
+        start-sleep -m 20
+        $SendMouseClick::mouse_event(0x00000010, 0, 0, 0, 0);
+}
+
+#use case:
+#Click-MouseButtonRight
+
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Window {
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool ClientToScreen(IntPtr hWnd, ref POINT lpPoint);
+}
+public struct RECT
+{
+    public int Left;
+    public int Top;
+    public int Right;
+    public int Bottom;
+}
+public struct POINT
+{
+    public int x;
+    public int y;
+}
+"@
+
+function getPosAndSize{
+param($windowName)
+$Handle = (Get-Process -Name $windowName).MainWindowHandle
+$ClientRect = New-Object RECT
+$Position = New-Object POINT
+$Size = New-Object POINT
+
+[Window]::GetClientRect($Handle, [ref]$ClientRect) | out-null
+$Position.x = 0 # $ClientRect.Left is always 0
+$Position.y = 0 # $ClientRect.Top
+$Size.x = $ClientRect.Right
+$Size.y = $ClientRect.Bottom
+[Window]::ClientToScreen($Handle, [ref]$Position) | out-null
+
+$PosAndSize = @($($Position.x), $($Position.y), $($Size.x), $($Size.y))
+return $PosAndSize
+}
+#use case:
+#getPosAndSize -windowName LeagueClientUx
+
+
+#Main Script
+$inputTime = Read-Host -Prompt '輸入投降時間(分鐘，不輸入為掛到10分鐘):'
+$inputRounds = Read-Host -Prompt '輸入重複回數(不輸入為掛到上限50次):'
+if(($inputTime -eq 0) -or ($inputTime -eq "")){$inputTime = 10}
+if(($inputRounds -gt 50) -or ($inputRounds -eq "")){
+$inputRounds = 50
+"已更改為上限50回"}
+$timeout = new-timespan -Minutes $inputTime
+$roundCount = 1
+
+:skipAll while ($roundCount -le $inputRounds){
+    "Start loop $inputRounds, Looking for Client..."
+    if (!($processClient)){
+		$processClient = Get-Process | Where-Object {$_.ProcessName -eq $targetClient}
+        "Client not found, please open Client and ready in lobby first!"
+        Break skipAll
+    }
+    "Client found"
+	if ($processClient){
+		"auto accept process start!"
+        while(!($processGame)){
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if($processGame){Break}
+            Show-Window "LeagueClientUx"
+            $currentWindow = getPosAndSize -windowName LeagueClientUx
+            [system.windows.forms.cursor]::Position = New-Object system.drawing.point($($queueStart[0]+$currentWindow[0]), $($queueStart[1]+$currentWindow[1]))
+            Show-Window "LeagueClientUx"
+            Click-MouseButtonLeft
+            start-sleep -s 1
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if($processGame){Break}
+            Show-Window "LeagueClientUx"
+            $currentWindow = getPosAndSize -windowName LeagueClientUx
+            [system.windows.forms.cursor]::Position = New-Object system.drawing.point($($queueAccept[0]+$currentWindow[0]), $($queueAccept[1]+$currentWindow[1]))
+            Show-Window "LeagueClientUx"
+            Click-MouseButtonLeft
+            start-sleep -s 2
+        }
+        "auto accept process finished, auto surrender process start!"
+        $timesup = [diagnostics.stopwatch]::StartNew()
+        while($timesup.elapsed -lt $timeout){
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            #buy chessman
+            moveCursorToSpot -targetX $slot1[0] -targetY $slot1[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -m 200
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $slot2[0] -targetY $slot2[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -m 200
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $slot3[0] -targetY $slot3[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -m 200
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $slot4[0] -targetY $slot4[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -m 200
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $slot5[0] -targetY $slot5[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -s 1
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            #collect loot
+            moveCursorToSpot -targetX $battlefield1[0] -targetY $battlefield1[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonRight
+            start-sleep -s 5
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $battlefield2[0] -targetY $battlefield2[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonRight
+            start-sleep -s 5
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $battlefield3[0] -targetY $battlefield3[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonRight
+            start-sleep -s 5
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $battlefield4[0] -targetY $battlefield4[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonRight
+            start-sleep -s 1
+            }
+        while($true){
+            #surrender
+            moveCursorToSpot -targetX $buttonConfig[0] -targetY $buttonConfig[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -s 1
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $buttonSurrend[0] -targetY $buttonSurrend[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -s 1
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $buttonConfirm[0] -targetY $buttonConfirm[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -s 1
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            moveCursorToSpot -targetX $buttonCancel[0] -targetY $buttonCancel[1]
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            Click-MouseButtonLeft
+            start-sleep -s 1
+            $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
+            if(!($processGame)){Break}
+            #will stop when game closed
+        }
+	}
+    $roundCount++
+}
+Read-Host "Press Enter to exit..."
+exit
