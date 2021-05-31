6079f5dec91a8a233390a63359a0e0fc7ab1173f
NicholasWu <james12313@gmail.com>
2021-05-31 08:39:32 +0800

    All Resolution update
    only support Garena TFT

--- clickBroSS.ps1
+++ clickBroSS.ps1
@@ -13,25 +13,29 @@
 #League of Legends (TM) Client
 
 #coordinates:
-#$coordClient =  @( 160 , 70 )
-$sizeClient =    @( 1600, 900 )
-$queueStart =    @( 728 , 841 )
-$queueAccept =   @( 790 , 700 )
-#$coordGame =    @( 0   , 0 )
-$sizeGame =      @( 1920, 1080 )
-$slot1 =         @( 570 , 1000 )
-$slot2 =         @( 784 , 1000 )
-$slot3 =         @( 982 , 1000 )
-$slot4 =         @( 1187, 1000 )
-$slot5 =         @( 1384, 1000 )
-$buttonConfig =  @( 1904, 886 )
-$buttonSurrend = @( 767 , 845 )
-$buttonConfirm = @( 835 , 485 )
-$buttonCancel =  @( 1289, 846 )
-$battlefield1 =  @( 1306, 651 )
-$battlefield2 =  @( 1304, 237 )
-$battlefield3 =  @( 598 , 240 )
-$battlefield4 =  @( 1306, 651 )
+#Example = @( windowAxisX, windowAxisY, windowSizeX, windowSizeY, "ProcessName")
+$garenaStart =   @( 480, 691, 1022, 718, "Garena" )
+$garenaClose =   @( 633, 407, 1022, 718, "Garena" )
+
+$queueStart =    @( 728, 841, 1600, 900, "LeagueClientUx" )
+$queueAccept =   @( 790, 700, 1600, 900, "LeagueClientUx" )
+$restartLobby =  @( 149, 51 , 1600, 900, "LeagueClientUx" )
+$restartMode =   @( 780, 274, 1600, 900, "LeagueClientUx" )
+$restartNormal = @( 679, 620, 1600, 900, "LeagueClientUx" )
+
+$slot1 =         @( 570 , 1000, 1920, 1080, "League of Legends" )
+$slot2 =         @( 784 , 1000, 1920, 1080, "League of Legends" )
+$slot3 =         @( 982 , 1000, 1920, 1080, "League of Legends" )
+$slot4 =         @( 1187, 1000, 1920, 1080, "League of Legends" )
+$slot5 =         @( 1384, 1000, 1920, 1080, "League of Legends" )
+$buttonConfig =  @( 1904, 886, 1920, 1080, "League of Legends" )
+$buttonSurrend = @( 767 , 845, 1920, 1080, "League of Legends" )
+$buttonConfirm = @( 835 , 485, 1920, 1080, "League of Legends" )
+$buttonCancel =  @( 1289, 846, 1920, 1080, "League of Legends" )
+$battlefield1 =  @( 1306, 651, 1920, 1080, "League of Legends" )
+$battlefield2 =  @( 1304, 237, 1920, 1080, "League of Legends" )
+$battlefield3 =  @( 598 , 240, 1920, 1080, "League of Legends" )
+$battlefield4 =  @( 1306, 651, 1920, 1080, "League of Legends" )
 
 #don't know what this does but can't delete
 [System.Reflection.Assembly]::LoadWithPartialName("system.windows.forms")
@@ -77,15 +81,23 @@ function Show-Window {
 #Show-Window "LeagueClientUx"
 
 function moveCursorToSpot{
-param($targetX, $targetY, $target)
-for($i=100;$i -ge 1;$i--){
-$Position = [system.windows.forms.cursor]::Position
-$PositionChangeX = ($targetX - $Position.x)/$i
-$PositionChangeY = ($targetY - $Position.y)/$i
+param($target)
 
-[system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Position.x + $PositionChangeX), ($Position.y + $PositionChangeY))
+$currWindow = getPosAndSize -windowName $target[4]
+"$currWindow"
+$weightX = $currWindow[2]/$target[2]
+$weightY = $currWindow[3]/$target[3]
 
-if($i -ge 2){start-sleep -m 5}
+Show-Window $target[4]
+
+for($i=20;$i -ge 1;$i--){
+    $Position = [system.windows.forms.cursor]::Position
+    $PositionChangeX = (($target[0] * $weightX) - $Position.x)/$i
+    $PositionChangeY = (($target[1] * $weightY) - $Position.y)/$i
+
+    [system.windows.forms.cursor]::Position = New-Object system.drawing.point(($Position.x + $PositionChangeX + $currWindow[0]), ($Position.y + $PositionChangeY + $currWindow[1]))
+
+    if($i -ge 2){start-sleep -m 25}
 }
 start-sleep -m 20
 }
@@ -164,7 +176,7 @@ $Size.x = $ClientRect.Right
 $Size.y = $ClientRect.Bottom
 [Window]::ClientToScreen($Handle, [ref]$Position) | out-null
 
-$PosAndSize = @($($Position.x), $($Position.y), $($Size.x), $($Size.y))
+$PosAndSize = @($Position.x, $Position.y, $Size.x, $Size.y)
 return $PosAndSize
 }
 #use case:
@@ -174,11 +186,18 @@ return $PosAndSize
 #Main Script
 $inputTime = Read-Host -Prompt '輸入投降時間(分鐘，不輸入為掛到10分鐘):'
 $inputRounds = Read-Host -Prompt '輸入重複回數(不輸入為掛到上限50次):'
-if(($inputTime -eq 0) -or ($inputTime -eq "")){$inputTime = 10}
+$inputRestartTime = Read-Host -Prompt '你英雄聯盟客戶端的開啟時間(秒，不輸入即設置為60秒，建議設定較長時間):'
+
+if(($inputTime -eq 0) -or ($inputTime -eq "")){
+$inputTime = 10}else{
+$timeout = new-timespan -Minutes $inputTime}
 if(($inputRounds -gt 50) -or ($inputRounds -eq "")){
 $inputRounds = 50
 "已更改為上限50回"}
-$timeout = new-timespan -Minutes $inputTime
+if($inputRestartTime -eq ""){$inputRestartTime = 60}
+
+$overQueueTime = new-timespan -Minutes 20
+$skipOnce = $false
 $roundCount = 1
 
 :skipAll while ($roundCount -le $inputRounds){
@@ -191,7 +210,39 @@ $roundCount = 1
     "Client found"
 	if ($processClient){
 		"auto accept process start!"
-        while(!($processGame)){
+        $overqueue = [diagnostics.stopwatch]::StartNew()
+        :skipQueue while(!($processGame)){
+            $processClient = Get-Process | Where-Object {$_.ProcessName -eq $targetClient}
+            if($overqueue.elapsed -gt $overQueueTime){
+                $processClient | Stop-Process -Force
+                Show-Window "Garena"
+                start-sleep -s 5
+                $currentWindow = getPosAndSize -windowName Garena
+                [system.windows.forms.cursor]::Position = New-Object system.drawing.point($($garenaStart[0]+$currentWindow[0]), $($garenaStart[1]+$currentWindow[1]))
+                Click-MouseButtonLeft
+                start-sleep -s 2
+                $currentWindow = getPosAndSize -windowName Garena
+                [system.windows.forms.cursor]::Position = New-Object system.drawing.point($($garenaClose[0]+$currentWindow[0]), $($garenaClose[1]+$currentWindow[1]))
+                Click-MouseButtonLeft
+                start-sleep -s $inputRestartTime
+
+                Show-Window "LeagueClientUx"
+                $currentWindow = getPosAndSize -windowName LeagueClientUx
+                [system.windows.forms.cursor]::Position = New-Object system.drawing.point($($restartLobby[0]+$currentWindow[0]), $($restartLobby[1]+$currentWindow[1]))
+                Click-MouseButtonLeft
+                start-sleep -s 5
+                $currentWindow = getPosAndSize -windowName LeagueClientUx
+                [system.windows.forms.cursor]::Position = New-Object system.drawing.point($($restartMode[0]+$currentWindow[0]), $($restartMode[1]+$currentWindow[1]))
+                Click-MouseButtonLeft
+                start-sleep -s 5
+                $currentWindow = getPosAndSize -windowName LeagueClientUx
+                [system.windows.forms.cursor]::Position = New-Object system.drawing.point($($restartNormal[0]+$currentWindow[0]), $($restartNormal[1]+$currentWindow[1]))
+                Click-MouseButtonLeft
+                start-sleep -s 5
+                $skipOnce = $true
+                Break skipQueue
+            }
+
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if($processGame){Break}
             Show-Window "LeagueClientUx"
@@ -209,41 +260,43 @@ $roundCount = 1
             Click-MouseButtonLeft
             start-sleep -s 2
         }
+        if($skipOnce -eq $false){
         "auto accept process finished, auto surrender process start!"
         $timesup = [diagnostics.stopwatch]::StartNew()
+        start-sleep -s 10
         while($timesup.elapsed -lt $timeout){
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             #buy chessman
-            moveCursorToSpot -targetX $slot1[0] -targetY $slot1[1]
+            moveCursorToSpot -target $slot1
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
             start-sleep -m 200
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $slot2[0] -targetY $slot2[1]
+            moveCursorToSpot -target $slot2
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
             start-sleep -m 200
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $slot3[0] -targetY $slot3[1]
+            moveCursorToSpot -target $slot3
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
             start-sleep -m 200
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $slot4[0] -targetY $slot4[1]
+            moveCursorToSpot -target $slot4
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
             start-sleep -m 200
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $slot5[0] -targetY $slot5[1]
+            moveCursorToSpot -target $slot5
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
@@ -251,57 +304,57 @@ $roundCount = 1
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             #collect loot
-            moveCursorToSpot -targetX $battlefield1[0] -targetY $battlefield1[1]
+            moveCursorToSpot -target $battlefield1
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonRight
             start-sleep -s 5
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $battlefield2[0] -targetY $battlefield2[1]
+            moveCursorToSpot -target $battlefield2
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonRight
             start-sleep -s 5
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $battlefield3[0] -targetY $battlefield3[1]
+            moveCursorToSpot -target $battlefield3
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonRight
             start-sleep -s 5
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $battlefield4[0] -targetY $battlefield4[1]
+            moveCursorToSpot -target $battlefield4
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonRight
             start-sleep -s 1
-            }
+        }
         while($true){
             #surrender
-            moveCursorToSpot -targetX $buttonConfig[0] -targetY $buttonConfig[1]
+            moveCursorToSpot -target $buttonConfig
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
             start-sleep -s 1
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $buttonSurrend[0] -targetY $buttonSurrend[1]
+            moveCursorToSpot -target $buttonSurrend
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
             start-sleep -s 1
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $buttonConfirm[0] -targetY $buttonConfirm[1]
+            moveCursorToSpot -target $buttonConfirm
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
             start-sleep -s 1
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
-            moveCursorToSpot -targetX $buttonCancel[0] -targetY $buttonCancel[1]
+            moveCursorToSpot -target $buttonCancel
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             Click-MouseButtonLeft
@@ -309,9 +362,13 @@ $roundCount = 1
             $processGame = Get-Process | Where-Object {$_.ProcessName -eq $targetGame}
             if(!($processGame)){Break}
             #will stop when game closed
-        }
+        }}
 	}
+    if($skipOnce -eq $false){
     $roundCount++
+    }elseif($skipOnce -eq $true){
+    $skipOnce = $false
+    }
 }
 Read-Host "Press Enter to exit..."
 exit
